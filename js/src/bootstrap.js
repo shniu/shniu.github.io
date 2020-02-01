c30a642ee7d2f9c175b32eccb801a44858bd4ea1
shniu <niushaohan@digcredit.com>
2020-02-01 21:55:56 +0800

    Site updated: 2020-02-01 21:55:55

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -1,41 +1,34 @@
-/* global NexT, CONFIG */
+/* global NexT: true */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   $(document).trigger('bootstrap:before');
 
-  /**
-   * Register JS handlers by condition option.
-   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
-   */
-  CONFIG.fastclick && NexT.utils.isMobile() && window.FastClick.attach(document.body);
-  CONFIG.lazyload && NexT.utils.lazyLoadPostsImages();
+  NexT.utils.isMobile() && window.FastClick.attach(document.body);
+
+  NexT.utils.lazyLoadPostsImages();
 
   NexT.utils.registerESCKeyEvent();
 
   NexT.utils.registerBackToTop();
 
-  // Mobile top menu bar.
-  $('.site-nav-toggle button').on('click', function() {
+  $('.site-nav-toggle button').on('click', function () {
     var $siteNav = $('.site-nav');
     var ON_CLASS_NAME = 'site-nav-on';
     var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
     var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
     var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
 
-    $siteNav.stop()[animateAction]('fast', function() {
+    $siteNav.stop()[animateAction]('fast', function () {
       $siteNav[animateCallback](ON_CLASS_NAME);
     });
   });
 
-  /**
-   * Register JS handlers by condition option.
-   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
-   */
-  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
-  CONFIG.tabs && NexT.utils.registerTabsTag();
 
+  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
   NexT.utils.embeddedVideoTransformer();
+  NexT.utils.addActiveClassToMenuItem();
+
 
   // Define Motion Sequence.
   NexT.motion.integrator
@@ -47,7 +40,7 @@ $(document).ready(function() {
   $(document).trigger('motion:before');
 
   // Bootstrap Motion.
-  CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
+  CONFIG.motion && NexT.motion.integrator.bootstrap();
 
   $(document).trigger('bootstrap:after');
 });
