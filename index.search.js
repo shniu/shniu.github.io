880ffe0aab76164cb480e1d9f17c39de49935e9f
shniu <xiaoniu198903@126.com>
2023-05-05 03:27:39 +0000

    deploy: shniu/insights@c40b182b1179f79275a4e4bf746e14503655d07c

---
+++ index.search.js
@@ -0,0 +1,18 @@
+var relearn_search_index=[{content:`bbbb….
+L1 network l1
+L2 network l2
+`,description:"",tags:null,title:"What is blockchain",uri:"/blockchain/0-introduction/index.html"},{content:`CS, Computer Science, 计算机科学是系统性研究信息与计算的理论基础以及它们在计算机系统中如何实现与应用的实用技术的学科；涵盖的内容非常多，但是最核心最基础的学科是计算机科学的基石，包括但不限于：
+数据结构与算法 计算机组成原理（计算机体系结构与计算机工程） 操作系统 计算机网络 分布式系统 计算机安全和密码学 数据库 并行计算和分布式计算 编译原理 软件设计 … `,description:"",tags:["CS"],title:"CS",uri:"/basics/index.html"},{content:`Blockchain is a distributed ledger technology that allows for the creation of a decentralized network of computers that can store and share information in a secure. Blockchain is a distributed ledger technology that allows for the creation of a decentralized network of computers that can store and share information in a secure. Blockchain is a distributed ledger technology that allows for the creation of a decentralized network of computers that can store and share information in a secure.
+What is blockchain L0 Layerzero `,description:"",tags:["Blockchain"],title:"Blockchain",uri:"/blockchain/index.html"},{content:`这是个人博客，汇总整理知识。
+aaaa
+public static void main(String[] args) { } hhh, aaabbb, =, "
+That’s some text with a footnote1
+That’s some more text with a footnote.2
+And that’s the footnote. ↩︎
+Anything of interest goes here.
+Blue light glows blue. ↩︎
+`,description:"",tags:null,title:"Overview",uri:"/index.html"},{content:`L0 Layerzero
+aaa agegeg
+bbb wefwef
+abddd `,description:"",tags:null,title:"L0 Layerzero",uri:"/blockchain/l0-layerzero/index.html"},{content:"",description:"",tags:null,title:"Blockchain",uri:"/tags/blockchain/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:"",description:"",tags:null,title:"CS",uri:"/tags/cs/index.html"},{content:`Projects…
+`,description:"",tags:null,title:"My Projects",uri:"/more/projects/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"}]
