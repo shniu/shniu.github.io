8d605f9156a53badefd70196bfbe312d77a816fc
shniu <xiaoniu198903@126.com>
2023-05-17 07:23:08 +0000

    deploy: shniu/insights@d242e248a2e90a4d75aaa1c375b2a12ce42ca101

--- index.search.js
+++ index.search.js
@@ -2,16 +2,15 @@ var relearn_search_index=[{content:`bbbb….
 L1 network l1
 L2 network l2
 `,description:"",tags:null,title:"What is blockchain",uri:"/blockchain/0-introduction/index.html"},{content:`CS, Computer Science, 计算机科学是系统性研究信息与计算的理论基础以及它们在计算机系统中如何实现与应用的实用技术的学科；涵盖的内容非常多，但是最核心最基础的学科是计算机科学的基石，包括但不限于：
-数据结构与算法 计算机组成原理（计算机体系结构与计算机工程） 操作系统 计算机网络 分布式系统 计算机安全和密码学 数据库 并行计算和分布式计算 编译原理 软件设计 … `,description:"",tags:["CS"],title:"CS",uri:"/basics/index.html"},{content:`Blockchain is a distributed ledger technology that allows for the creation of a decentralized network of computers that can store and share information in a secure. Blockchain is a distributed ledger technology that allows for the creation of a decentralized network of computers that can store and share information in a secure. Blockchain is a distributed ledger technology that allows for the creation of a decentralized network of computers that can store and share information in a secure.
-What is blockchain L0 Layerzero `,description:"",tags:["Blockchain"],title:"Blockchain",uri:"/blockchain/index.html"},{content:`这是个人博客，汇总整理知识。
-aaaa
-public static void main(String[] args) { } hhh, aaabbb, =, "
-That’s some text with a footnote1
-That’s some more text with a footnote.2
-And that’s the footnote. ↩︎
-Anything of interest goes here.
-Blue light glows blue. ↩︎
-`,description:"",tags:null,title:"Overview",uri:"/index.html"},{content:`L0 Layerzero
+数据结构与算法 计算机组成原理（计算机体系结构与计算机工程） 操作系统 计算机网络 分布式系统 计算机安全和密码学 数据库 并行计算和分布式计算 编译原理 软件设计 … `,description:"",tags:["CS"],title:"CS",uri:"/basics/index.html"},{content:`Blockchain is a distributed ledger technology that allows for the creation of a decentralized network of computers that can store and share information in a secure.
+怎么组织区块链相关的知识呢？
+区块链原理：技术原理、经济模型等，以及常见的主网 Web3 热点 (Hot / Trend) BRC20 What is blockchain Blockchain Principle Smart Contracts L0 Layerzero `,description:"",tags:["Blockchain"],title:"Blockchain",uri:"/blockchain/index.html"},{content:`区块链原理
+双花问题 Markel Tree 交易模型 最基础的两个：UTXO 和 账户模型
+`,description:"",tags:null,title:"Blockchain Principle",uri:"/blockchain/principle/index.html"},{content:`这里主要是关于智能合约。
+`,description:"",tags:null,title:"Smart Contracts",uri:"/blockchain/smart-contracts/index.html"},{content:`大道至简，简则易行
+学习不仅是为了找到答案，更是为了找到方法。 计算机科学领域的任何问题都可以通过增加一个间接的中间层来解决。 简单的设计是：设计者确定其设计已经达到了完美的标准不是不能再增加任何东西，而是不能再减少任何东西。 – 编程珠玑 程序员的主要问题与其说是技术问题，还不如说是心理问题，他不能解决问题，是因为他在企图解决错误问题，问题的最终解决， 是通过打破他的概念壁垒，进而去解决一个较简单的问题而实现的。 – 编程珠玑 如无必要，勿增实体。 – 奥卡姆剃刀原理 成为一名适应时代变化的软件工程师，知识体系的建设至关重要。
+Quick Reference CS Blockchain How to organize 指导思想是以计算机基础知识为底座，并结合人类在各方面留下的宝贵经验和真理，延伸到更多的应用领域，注重实战。
+基础知识 操作系统原理 网络原理 计算机组成原理 软件工程 编程语言 设计模式 架构设计 代码质量 代码重构 代码审查 代码测试 代码部署 代码发布 代码监控 代码运维 中间件 数据库 缓存 消息队列 搜索引擎 分布式文件系统 分布式数据库 分布式缓存 分布式消息队列 分布式搜索引擎 分布式事务 分布式锁 分布式调度 分布式配置 分布式日志 分布式监控 分布式追踪 分布式链路 分布式存储 分布式计算 分布式任务 分布式协调 topic 行业 区块链 人工智能 云计算 `,description:"",tags:null,title:"Overview",uri:"/index.html"},{content:`L0 Layerzero
 aaa agegeg
 bbb wefwef
 abddd `,description:"",tags:null,title:"L0 Layerzero",uri:"/blockchain/l0-layerzero/index.html"},{content:"",description:"",tags:null,title:"Blockchain",uri:"/tags/blockchain/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:"",description:"",tags:null,title:"CS",uri:"/tags/cs/index.html"},{content:`Projects…
